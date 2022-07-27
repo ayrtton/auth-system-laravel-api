b41e203daa6d63eb902c33988803c8adc309845a
Ayrton <ayrtton.vinicius@gmail.com>
2022-07-27 15:19:50 -0300

    Create user list endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('users', [UserController::class, 'index']);
+    });
 });
