db99bc1de7820f10b462c432ad813ef2eb22dbc0
Ayrton <ayrtton.vinicius@gmail.com>
2022-08-18 16:20:00 -0300

    Set the reset password email design

--- resources/views/mail/reset.blade.php
+++ resources/views/mail/reset.blade.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Reset password</title>
-    </head>
-    <body>
-        Change your password <a href="">Click here</a><br/>
-        Pincode: {{ $token }}
-    </body>
-</html>
+@component('mail::message')
+<h1>Hello!</h1>
+Your token is {{ $token }}. <br />
+Please click the button below to reset your password.
+@component('mail::button', ['url' => ''])
+Reset Password
+@endcomponent
+@endcomponent
