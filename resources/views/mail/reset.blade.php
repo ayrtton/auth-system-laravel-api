6e5df31c333eb307ec84a7344ec0720d128b21dc
Ayrton <ayrtton.vinicius@gmail.com>
2022-08-26 19:28:32 -0300

    Redirect user to password reset page

--- resources/views/mail/reset.blade.php
+++ resources/views/mail/reset.blade.php
@@ -2,7 +2,7 @@
 <h1>Hello!</h1>
 Your token is {{ $token }}. <br />
 Please click the button below to reset your password.
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' => 'https://login-signup-system.vercel.app/password/reset'])
 Reset Password
 @endcomponent
 @endcomponent
